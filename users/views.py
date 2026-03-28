9dadb0e8172d709ddcdb495ddc6b764b69c087aa
Salar <lrdsallymail@gmail.com>
2026-03-28 07:23:25 +0330

    authorization rooms

--- users/views.py
+++ users/views.py
@@ -5,9 +5,11 @@ from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 
 from users.serializers import *
+from users.utils import permissions as cp
 from debts.serializers import DebtSerializer
 # Create your views here.
 
@@ -16,7 +18,6 @@ User = get_user_model()
 
 class RoomView(ModelViewSet):
     queryset = Room.objects.all()
-    # permission_classes = [IsAuthenticated]
     serializer_class = RoomSerializer
     lookup_field = "slug"
 
@@ -46,3 +47,15 @@ class RoomView(ModelViewSet):
         context =  super().get_serializer_context()
         context["view"] = self
         return context
+
+    def get_permissions(self):
+        if self.action in ["retrieve", "debt_list"]:
+            permission_classes = [cp.IsMember]
+        
+        elif self.action == "list":
+            permission_classes = [IsAuthenticated]
+        
+        elif self.action in ["update", "partial_update", "destroy"]:
+            permission_classes = [cp.IsRoomAdmin]
+        
+        return [permission() for permission in permission_classes]
