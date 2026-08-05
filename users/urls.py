fa73fb6b47439967f4bf5a9b33de84faa6038a5a
Salar <lrdsallymail@gmail.com>
2026-08-06 00:16:42 +0330

    fix: fix notifical url path (The APIView was considered as a viewset)

--- users/urls.py
+++ users/urls.py
@@ -6,12 +6,12 @@ from users.views import UserView, LoginView, RegisterView, NotificationView, Roo
 
 router = DefaultRouter()
 router.register(r'users', UserView, basename="users")
-router.register(r'notifications', NotificationView, basename='notifications')
 router.register(r'rooms', RoomView, basename="rooms")
 
 
 urlpatterns = [
     path('', include(router.urls)),   
     path('users/login', LoginView.as_view(), name='login'),
-    path('users/register', RegisterView.as_view(), name="register") 
+    path('users/register', RegisterView.as_view(), name="register"),
+    path('notifications/', NotificationView.as_view(), name="notifications")
 ]
